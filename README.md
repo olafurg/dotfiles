ed11565d783ec2970f00d86e489e46cdaad90356
Ólafur Guðmundsson <olafur.g@gmail.com>
2015-07-22 19:08:25 +0200

    Modify script, README.md and add .gitconfig.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Dotfiles collection
-Backing up my dotfiles, using this guide: http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
+Backing up my dotfiles, using this guide: 
+
+* http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
+
+Also using `git-aware-prompt`, from here:
+
+* https://github.com/jimeh/git-aware-prompt
+
+Everything should be included in this script though.
+
+## How to set up
+On a newly set up machine here's how to set up and use the repo.
+
+1. Check out the repository to your home directory.
+
+  `git clone <repo-link>`
+
+  You should have the dotfiles directory in `/home/username/dotfiles`.
+
+2. `cd` into the repository and run `./makesymlinks.sh`
+
+That's it. Restart the command line and you should be good.
+
+## Adding new files to monitor
+If you're adding new dotfiles make sure to add them to the list in `makesymlinks.sh`.
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,9 +6,9 @@
 
 ########## Variables
 
-dir=~/.dotfiles                                  # dotfiles directory
-olddir=~/.dotfiles_old                           # old dotfiles backup directory
-files=".vimrc .vim .bashrc .bash"    # list of files/folders to symlink in homedir
+dir=~/dotfiles                                  # dotfiles directory
+olddir=~/dotfiles_old                           # old dotfiles backup directory
+files=".vimrc .vim .bashrc .bash .gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -17,17 +17,17 @@ echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
 echo "done"
 
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
+# create (if not there) and change to the dotfiles directory
+echo -n "Change to the dotfiles directory ..."
 cd $dir
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
 echo "Moving any existing dotfiles from ~ to $olddir"
-mv ~/$file $olddir
-echo "Creating symlink to $file in home directory."
-ln -s $dir/$file ~/$file
+for file in $files; do
+	mv ~/$file $olddir
+	echo "Creating symlink to $file in home directory."
+	ln -s $dir/$file ~/$file
 done
 
 install_zsh () {
