3759eb4a8abe1e78de41be1769313a1e9f960008
Ólafur Guðmundsson <olafur.g@gmail.com>
2016-03-27 13:47:55 +0200

    Add Terminator config file to the list.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@ dir=~/dotfiles
 olddir=~/dotfiles_old
 
 # List of files/dirs to symlink in home dir (without leading dot)
-files="vimrc vim bashrc bash gitconfig gemrc rspec"
+files="vimrc vim bashrc bash gitconfig gemrc rspec config/terminator/config"
 
 ########## Setup
 
