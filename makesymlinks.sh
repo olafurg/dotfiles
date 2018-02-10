be6d1cee7085ee23c695fea5aa6cf6193ba9d503
Ólafur Guðmundsson <olafur.g@gmail.com>
2018-02-10 17:15:12 +0100

    Change how zsh and oh-my-zsh are installed. Format improvement.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -38,29 +38,27 @@ done
 echo "done"
 
 install_zsh () {
-# Test to see if zshell is installed.  If it is:
+  # Test to see if zsh is installed.
 	if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-# Clone the official oh-my-zsh repository from GitHub only if it isn't already present
-		if [[ ! -d $dir/oh-my-zsh/ ]]; then
-			git clone https://github.com/robbyrussell/oh-my-zsh.git
-				fi
-# Set the default shell to zsh if it isn't currently set to zsh
-				if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-					chsh -s $(which zsh)
-						fi
-				else
-# If zsh isn't installed, get the platform of the current machine
-					platform=$(uname);
-# If the platform is Linux, try an apt-get to install zsh and then recurse
-	if [[ $platform == 'Linux' ]]; then
-		sudo apt-get install zsh
+    # Install oh-my-zsh acc. to their instructions.
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    # Set the default shell to zsh if it isn't currently set to zsh
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+      chsh -s $(which zsh)
+    fi
+  else
+    # If zsh isn't installed, get the platform of the current machine
+    platform=$(uname);
+    # If the platform is Linux, try an apt-get to install zsh and then recurse
+    if [[ $platform == 'Linux' ]]; then
+      sudo apt-get install zsh
 			install_zsh
-# If the platform is OS X, tell the user to install zsh :)
-			elif [[ $platform == 'Darwin' ]]; then
+    # If the platform is OS X, tell the user to install zsh :)
+    elif [[ $platform == 'Darwin' ]]; then
 			echo "Please install zsh, then re-run this script!"
 			exit
-			fi
-			fi
+    fi
+  fi
 }
 
 install_zsh
